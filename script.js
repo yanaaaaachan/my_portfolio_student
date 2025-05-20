882198d12b13ad96a65044ed570685ca91b4844a
yanaaaaachan <147154485+yanaaaaachan@users.noreply.github.com>
2025-05-20 23:16:44 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-document.addEventListener("DOMContentLoaded", () =>{
-    let Menu1=document.getElementById("menu1")
-    let Menu2=document.getElementById("menu2")
-    let Menu3=document.getElementById("menu3")
-    let Menu4=document.getElementById("menu4")
+// document.addEventListener("DOMContentLoaded", () =>{
+//     let Menu1=document.getElementById("menu1")
+//     let Menu2=document.getElementById("menu2")
+//     let Menu3=document.getElementById("menu3")
+//     let Menu4=document.getElementById("menu4")
 
 
-    Menu1.addEventListener("mouseover",function(){
-        Menu1.style.color="gray"
-    })
-    Menu2.addEventListener("mouseover",function(){
-        Menu2.style.color="gray"
-    })
-    Menu3.addEventListener("mouseover",function(){
-        Menu3.style.color="gray"
-    })
-    Menu4.addEventListener("mouseover",function(){
-        Menu4.style.color="gray"
-    })
-    Menu1.addEventListener("mouseout",function(){
-        Menu1.style.color="#454846"
-    })
-    Menu2.addEventListener("mouseout",function(){
-        Menu2.style.color="#454846"
-    })
-    Menu3.addEventListener("mouseout",function(){
-        Menu3.style.color="#454846"
-    })
-    Menu4.addEventListener("mouseout",function(){
-        Menu4.style.color="#454846"
-    })
+//     Menu1.addEventListener("mouseover",function(){
+//         Menu1.style.color="gray"
+//     })
+//     Menu2.addEventListener("mouseover",function(){
+//         Menu2.style.color="gray"
+//     })
+//     Menu3.addEventListener("mouseover",function(){
+//         Menu3.style.color="gray"
+//     })
+//     Menu4.addEventListener("mouseover",function(){
+//         Menu4.style.color="gray"
+//     })
+//     Menu1.addEventListener("mouseout",function(){
+//         Menu1.style.color="#454846"
+//     })
+//     Menu2.addEventListener("mouseout",function(){
+//         Menu2.style.color="#454846"
+//     })
+//     Menu3.addEventListener("mouseout",function(){
+//         Menu3.style.color="#454846"
+//     })
+//     Menu4.addEventListener("mouseout",function(){
+//         Menu4.style.color="#454846"
+//     })
 
-});
+// });
