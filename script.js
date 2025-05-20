aa718a043436bdcf851394a5c7d0ac72f134f57c
yanaaaaachan <147154485+yanaaaaachan@users.noreply.github.com>
2025-05-20 23:12:13 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () =>{
-    //ヘッダーの要素の色をホバーで変える
     let Menu1=document.getElementById("menu1")
     let Menu2=document.getElementById("menu2")
     let Menu3=document.getElementById("menu3")
@@ -31,21 +30,4 @@ document.addEventListener("DOMContentLoaded", () =>{
         Menu4.style.color="#454846"
     })
 
-//画像を変える
-// let Like=document.getElementById("like")
-// let GoodAt=document.getElementById("good_at")
-// let Club=document.getElementById("club")
-// let Qualification=document.getElementById("qualification")
-// let LikeSpan=document.getElementById("like_span")
-// let GoodAtSpan=document.getElementById("good_at_span")
-// let ClubSpan=document.getElementById("club_span")
-// let QualificationSpan=document.getElementById("qualification_span")
-// GoodAt.addEventListener("mouseover",function(){
-//     GoodAtSpan.style.color="transparent"
-//     GoodAt.style.backgroundImage='url("good_at_img.jpg")'
-// })
-// GoodAt.addEventListener("mouseout",function(){
-//     GoodAtSpan.style.color="#DEAD6A"
-//     GoodAt.style.backgroundImage=''
-// })
 });
